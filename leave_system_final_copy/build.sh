6bc0691fc74dab776710f534f645900ef4c67e48
designbhutan <yumchen94@gmail.com>
2026-09-04 17:15:00 +0600

    Update build.sh

--- leave_system_final_copy/build.sh
+++ leave_system_final_copy/build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-python manage.py check --deploy
+# Navigate to the folder containing manage.py and requirements.txt
+cd leave_system_final_copy
+
+# Upgrade pip and install dependencies
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+# Run static assets collection and database migrations
+python manage.py collectstatic --no-input
+python manage.py migrate
